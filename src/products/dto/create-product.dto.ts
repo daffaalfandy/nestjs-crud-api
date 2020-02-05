35a19ce41e3dea45ea63123a1a19f71c1b76c4e5
Daffa Alfandy <53203773+daffaalfandy@users.noreply.github.com>
2020-02-05 20:15:04 +0700

    Add dependency class-validator, add ValidationPipe

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateProductDTO {
+    @IsNotEmpty()
+    @IsString()
     readonly id: string;
+    @IsNotEmpty()
+    @IsString()
     readonly name: string;
 }
